481e129b059c8ccfb7188e22340f6d02c3de3c1b
marcio lira <machollanda@gmail.com>
2021-01-06 22:25:32 -0300

    initial files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO.Serie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
